878262bad60b5514550503472f0610bbd010866e
Daniel Maricic <daniel@woss.io>
2023-06-03 14:12:36 +0200

    add daisyui, tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div
+	class="hero min-h-screen"
+	style="background-image: url(https://img.macula.link/ipfs/bafybeifz4vnqlhjzswj36jqnnohl5jmtyuzdb3pmzlypajqtvvf57q2zae/DSC02817.jpg);"
+>
+	<div class="hero-overlay bg-opacity-60" />
+	<div class="hero-content text-center text-neutral-content">
+		<div class="max-w-md">
+			<h1 class="mb-5 text-5xl font-bold">Welcome to SvelteKit</h1>
+			<p>
+				Visit <a class="btn btn-primary" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
+				documentation
+			</p>
+		</div>
+	</div>
+</div>
