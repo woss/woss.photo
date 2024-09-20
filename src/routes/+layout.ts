62c47e0c41ee27be9ffa063fbc7c23fc45ef4d56
Daniel Maricic <daniel@woss.io>
2024-09-20 12:54:13 +0200

    fix build

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,9 @@
 import { maculaInstance } from '$src/appStore.js';
 
+export const ssr = true;
 export const csr = true;
-export const prerender = false;
+export const prerender = true;
+export const trailingSlash = 'always';
 
 export async function load({ fetch }) {
   const res = await maculaInstance.me(fetch);
--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,26 @@
 import adapter from '@sveltejs/adapter-static';
 
-import vitePreprocess from 'svelte-preprocess';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
-
+  preprocess: sveltePreprocess({
+    postcss: true,
+    sourceMap: true
+  }),
   kit: {
     adapter: adapter({
-      // fallback: '200.html', // this is for the surge.sh
       fallback: '200.html',
-      precompress: true
+      precompress: true,
+      strict: false
     }),
     prerender: {
-      concurrency: 10
-    },
-    files: {
-      lib: 'src/components'
-    },
-    alias: {
-      $src: 'src',
-      '$src/*': 'src/*',
-      $lib: 'src/components',
-      '$lib/*': 'src/components/*'
+      // delete this line when Pricing page is ready
+      handleMissingId: 'ignore',
+      concurrency: 10,
+      crawl: true
     }
   }
 };
